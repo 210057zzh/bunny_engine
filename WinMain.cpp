a487ddc2eeb5f04e3abc17ba54c0ebdd8029b87f
David Zhang <210057zzh@gmail.com>
2023-04-24 04:34:24 -0700

    create window

---
+++ WinMain.cpp
@@ -0,0 +1,73 @@
+#include <Windows.h>
+
+const auto cWindowWidth = 1920;
+const auto cWindowHeight = 1080;
+
+LRESULT CALLBACK main_window_proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
+)
+{
+	const auto pClassName = L"bunny_engine_window";
+	// register window class
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = main_window_proc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = pClassName;
+	RegisterClassEx(&wc);
+
+	// create window
+	const auto hWindow = CreateWindowEx(
+		0,
+		pClassName,
+		L"bunny_engine",
+		WS_CAPTION | WS_MAXIMIZEBOX | WS_SYSMENU,
+		0,
+		0,
+		cWindowWidth,
+		cWindowHeight,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	ShowWindow(hWindow, SW_SHOW);
+
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+	return 0;
+}
+
+LRESULT main_window_proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	default:
+		break;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
